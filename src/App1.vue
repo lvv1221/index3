58bfb1e37dfee649e1b187f537c1488d3983bbc5
lvv1221 <liwwa@foxmail.com>
2017-05-13 22:14:25 +0800

    test
    测试下提交功能

--- src/App1.vue
+++ src/App1.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div>
-      <router-link to="/home">Go to Home</router-link>
-      <router-link to="/about">Go to About</router-link>
+      <router-link to="/home">Go to Home!!!</router-link>
+      <router-link to="/about">Go to About!!!</router-link>
     </div>
     <router-view></router-view>
   </div>
