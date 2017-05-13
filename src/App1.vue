b2ee8495dc2fb379eeb0218802c2171123a6ae83
lvv1221 <liwwa@foxmail.com>
2017-05-13 22:33:06 +0800

    delete .idea

--- src/App1.vue
+++ src/App1.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div>
       <router-link to="/home">Go to Home!!!</router-link>
-      <router-link to="/about">Go to About!!!</router-link>
+      <router-link to="/about">Go to About!!!!</router-link>
     </div>
     <router-view></router-view>
   </div>
