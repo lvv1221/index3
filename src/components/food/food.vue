63db904cf1332e14e11de95a28ac6cd605610628
lvv1221 <liwwa@foxmail.com>
2017-05-19 00:32:28 +0800

    5-19

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -32,6 +32,11 @@
           <h1 class="title">商品信息</h1>
           <p class="text">{{food.info}}</p>
         </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -42,17 +47,30 @@
   import BScroll from 'better-scroll'
   import cartcontrol from '../cartcontrol/cartcontrol.vue'
   import split from '../split/split.vue'
+  import ratingselect from '../ratingselect/ratingselect.vue'
+ //  const POSITIVE = 0
+  // const NEGATIVE = 1
+  const ALL = 2
   export default {
     name: 'food',
     props: ['food'],
     data () {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       }
     },
     methods: {
       show () {
         this.showFlag = true
+        this.selectType = ALL
+        this.onlyContent = true
         this.$nextTick(() => {
           if (!this.scroll) {
             this.srcoll = new BScroll(this.$refs.food, {
@@ -79,7 +97,8 @@
     },
     components: {
       cartcontrol,
-      split
+      split,
+      ratingselect
     }
   }
 </script>
@@ -183,4 +202,11 @@
         padding 0 8px
         font-size 12px
         color rgb(77, 85, 93)
+    .rating
+      padding-top 18px
+      .title
+        line-height 14px
+        margin-left 18px
+        font-size 14px
+        color rgb(7, 17, 27)
 </style>
