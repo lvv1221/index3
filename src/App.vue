d4ccc7ef79e9b197ee824006c54c7c4bb0466958
lvv1221 <liwwa@foxmail.com>
2017-05-21 23:21:27 +0800

    90%

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
 <script type="text/ecmascript-6">
   import header from './components/header/header.vue'
   import axios from 'axios'
+  import {urlParse} from './common/js/util'
   const ERR_OK = 0
   export default {
     name: 'app',
@@ -26,13 +27,22 @@
       'v-header': header
     },
     data: function () {
-      return {seller: ''}
+      return {
+        seller: {
+          id: (() => {
+            let queryParam = urlParse()
+            // console.log(JSON.stringify(queryParam))
+            return queryParam.id
+          })()// 立即执行函数
+        }
+      }
     },
     created: function () {
-      axios.get('/api/seller').then((res) => {
+      axios.get(`/api/seller?id=${this.seller.id}`).then((res) => {
         if (res.data.errno === ERR_OK) {
-          this.seller = res.data.data
-          // console.log(this.seller)
+         // this.seller = res.data.data
+          this.seller = Object.assign({}, this.seller, res.data.data)
+         // console.log(this.seller)
         }
       })
     }
@@ -48,14 +58,14 @@
       width: 100%
       height: 40px
       line-height: 40px
-      border-1px(rgba(7,17,27,0.1))
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
         flex: 1
         text-align: center
         & > a
           display: block
           font-size: 14px
-          color: rgb(77,85,93)
+          color: rgb(77, 85, 93)
           &.router-link-active
-           color: rgb(240,20,20)
+            color: rgb(240, 20, 20)
 </style>
